d423b3d0eb4caf859391e7071b01e80871d846ab
amirparvaneh <amirparvaneh138@gmail.com>
2024-07-26 14:20:45 +0330

    [feature] problem linkedList;

---
+++ src/algortihm/linkedlist/LinkedList.java
@@ -0,0 +1,37 @@
+package algortihm.linkedlist;
+
+public class LinkedList {
+
+    // CREATE CLASS VARIABLES, NODE CLASS, AND CONSTRUCTOR HERE //
+
+
+
+    public void printList() {
+        Node temp = head;
+        while (temp != null) {
+            System.out.println(temp.value);
+            temp = temp.next;
+        }
+    }
+
+    public void getHead() {
+        if (head == null) {
+            System.out.println("Head: null");
+        } else {
+            System.out.println("Head: " + head.value);
+        }
+    }
+
+    public void getTail() {
+        if (head == null) {
+            System.out.println("Tail: null");
+        } else {
+            System.out.println("Tail: " + tail.value);
+        }
+    }
+
+    public void getLength() {
+        System.out.println("Length: " + length);
+    }
+
+}
